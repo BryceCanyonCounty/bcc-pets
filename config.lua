498fc68ed17c1bc331ecee8f028b65a607560efb
AndrewR3K <andrew.martin.louis@gmail.com>
2022-05-26 00:03:42 -0700

    Adding boy and bullfrog

--- config.lua
+++ config.lua
@@ -36,125 +36,154 @@ Config.PetAttributes = {
 
 Config.Pets = {
     {
-        ['Text'] = "$200 - Husky",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 200,
-            ['Model'] = "A_C_DogHusky_01",
-            ['Level'] = 1
+        Text = "$30 - BullFrog",
+        SubText = "",
+        Desc = "Ribbet Ribbet",
+        Param = {
+            Price = 30,
+            Model = "a_c_frogbull_01",
+            Level = 1
+        }
+    },
+    {
+        Text = "$200 - Husky",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 200,
+            Model = "A_C_DogHusky_01",
+            Level = 1
         }
     },
     --[[ {
-        ['Text'] = "$50 - Mutt",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 50,
-            ['Model'] = "A_C_DogCatahoulaCur_01",
-            ['Level'] = 1
+        Text = "$50 - Mutt",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 50,
+            Model = "A_C_DogCatahoulaCur_01",
+            Level = 1
         }
         }, ]]
     {
-        ['Text'] = "$100 - Labrador Retriever",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 100,
-            ['Model'] = "A_C_DogLab_01",
-            ['Level'] = 1
+        Text = "$100 - Labrador Retriever",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 100,
+            Model = "A_C_DogLab_01",
+            Level = 1
         }
     },
     {
-        ['Text'] = "$100 - Rufus",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 100,
-            ['Model'] = "A_C_DogRufus_01",
-            ['Level'] = 1
+        Text = "$100 - Rufus",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 100,
+            Model = "A_C_DogRufus_01",
+            Level = 1
         }
     },
     {
-        ['Text'] = "$150 - Coon Hound",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 150,
-            ['Model'] = "A_C_DogBluetickCoonhound_01",
-            ['Level'] = 1
+        Text = "$150 - Coon Hound",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 150,
+            Model = "A_C_DogBluetickCoonhound_01",
+            Level = 1
         }
     },
         {
-        ['Text'] = "$150 - Hound Dog",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 150,
-            ['Model'] = "A_C_DogHound_01",
-            ['Level'] = 1
+        Text = "$150 - Hound Dog",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 150,
+            Model = "A_C_DogHound_01",
+            Level = 1
         }
     }, 
     {
-        ['Text'] = "$200 - Border Collie",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 200,
-            ['Model'] = "A_C_DogCollie_01",
-            ['Level'] = 1
+        Text = "$200 - Border Collie",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 200,
+            Model = "A_C_DogCollie_01",
+            Level = 1
         }
     },
     {
-        ['Text'] = "$200 - Poodle",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 200,
-            ['Model'] = "A_C_DogPoodle_01",
-            ['Level'] = 1
+        Text = "$200 - Poodle",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 200,
+            Model = "A_C_DogPoodle_01",
+            Level = 1
         }
     },
     
     {
-        ['Text'] = "$100 - Foxhound",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 100,
-            ['Model'] = "A_C_DogAmericanFoxhound_01",
-            ['Level'] = 1
+        Text = "$100 - Foxhound",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 100,
+            Model = "A_C_DogAmericanFoxhound_01",
+            Level = 1
         }
     },
     {
-        ['Text'] = "$100 - Australian Shephard",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 100,
-            ['Model'] = "A_C_DogAustralianSheperd_01",
-            ['Level'] = 1
+        Text = "$100 - Australian Shephard",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 100,
+            Model = "A_C_DogAustralianSheperd_01",
+            Level = 1
         }
     },
     {
-        ['Text'] = "$100 - Cat",
-        ['SubText'] = "",
-        ['Desc'] = "Best pet you'll ever have",
-        ['Param'] = {
-            ['Price'] = 100,
-            ['Model'] = "A_C_Cat_01",
-            ['Level'] = 1
+        Text = "$100 - Cat",
+        SubText = "",
+        Desc = "Best pet you'll ever have",
+        Param = {
+            Price = 100,
+            Model = "A_C_Cat_01",
+            Level = 1
         }
     },
-    
     {
-        ['Text'] = "$400 - Adopt a Child (girl)",
-        ['SubText'] = "",
-        ['Desc'] = "Poor child lost her mother in the fires.",
-        ['Param'] = {
-            ['Price'] = 400,
-            ['Model'] = "CS_GERMANDAUGHTER",
-            ['Level'] = 1
+        Text = "$400 - Adopt a Child (girl)",
+        SubText = "",
+        Desc = "Poor child lost her mother in the fires.",
+        Param = {
+            Price = 400,
+            Model = "CS_GERMANDAUGHTER",
+            Level = 1
         }
     },
+    {
+        Text = "$400 - Adopt a Child (boy)",
+        SubText = "",
+        Desc = "Poor child lost his parents.",
+        Param = {
+            Price = 400,
+            Model = "cs_germanson",
+            Level = 1
+        }
+    },
+    {
+        Text = "$400 - Adopt a Street Child",
+        SubText = "",
+        Desc = "Poor child grew up in the streets.",
+        Param = {
+            Price = 200,
+            Model = "a_m_y_nbxstreetkids_01",
+            Level = 1
+        }
+    }
 }
